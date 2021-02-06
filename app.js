f55a89375d457df4f5d7b03595b48c83fdd16257
sasha3nique <thxvolvo4steam@gmail.com>
2021-02-06 19:59:44 +0200

    colors

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ google.charts.setOnLoadCallback(drawBasic);
 function drawBasic(args=[0, 0.67, 3.63, 9.55, 15.68,  11.95, 9.42, 6.26, 4.66, 2.4, 0.58, 0.13, 0.36, 0.13, 0.36, 0.13, 0.1, 0, 0, 0, 0]) {
       var data = google.visualization.arrayToDataTable([
         ['VHI', '%', { role: 'style' }],
-        ['0', args[0], '#52b788'],
-        ['5', args[1], '#52b788'],
-        ['10', args[2], '#52b788'],
-        ['15', args[3], '#52b788'],
-        ['20', args[4], '#40916c'],
-        ['25', args[5], '#40916c'],
-        ['30', args[6], '#40916c'],
-        ['35', args[7], '#40916c'],
-        ['40', args[8],'#2d6a4f'],
-        ['45', args[9],'#2d6a4f'],
-        ['50', args[10],'#2d6a4f'],
-        ['55', args[11],'#2d6a4f'],
-        ['60', args[12],'#2d6a4f'],
-        ['65', args[13], '#1b4332'],
-        ['70', args[14], '#1b4332'],
-        ['75', args[15], '#1b4332'],
-        ['80', args[16], '#1b4332'],
-        ['85', args[17], '#1b4332'],
-        ['90', args[18], '#1b4332'],
-        ['95', args[19], '#1b4332'],
-        ['100', args[20], '#1b4332']
+        ['0', args[0], '#f8961e'],
+        ['5', args[1], '#f8961e'],
+        ['10', args[2], '#f8961e'],
+        ['15', args[3], '#f8961e'],
+        ['20', args[4], '#f9c74f'],
+        ['25', args[5], '#f9c74f'],
+        ['30', args[6], '#f9c74f'],
+        ['35', args[7], '#f9c74f'],
+        ['40', args[8],'#90be6d'],
+        ['45', args[9],'#90be6d'],
+        ['50', args[10],'#90be6d'],
+        ['55', args[11],'#90be6d'],
+        ['60', args[12],'#90be6d'],
+        ['65', args[13], '#43aa8b'],
+        ['70', args[14], '#43aa8b'],
+        ['75', args[15], '#43aa8b'],
+        ['80', args[16], '#43aa8b'],
+        ['85', args[17], '#43aa8b'],
+        ['90', args[18], '#43aa8b'],
+        ['95', args[19], '#43aa8b'],
+        ['100', args[20], '#43aa8b']
       ]);
       var options = {
         title: '',
@@ -39,7 +39,12 @@ function drawBasic(args=[0, 0.67, 3.63, 9.55, 15.68,  11.95, 9.42, 6.26, 4.66, 2
         height: 600,
         'chartArea': {'width': '80%', 'height': '80%'},
         allowCollapse: true,
-        legend: 'none'
+        legend: 'none',
+        animation:{
+          duration: 1000,
+          easing: 'out'
+        },
+        startup: true,
       };
       var chart = new google.visualization.ColumnChart(
         document.getElementById('chart_div'));
